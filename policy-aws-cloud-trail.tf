eadbfde28d833a00fa937b46f0fe2f091e9a9ace
groca-sysdig <105215194+groca-sysdig@users.noreply.github.com>
2023-02-13 15:38:49 +0100

    Update policy-aws-cloud-trail.tf

--- policy-aws-cloud-trail.tf
+++ policy-aws-cloud-trail.tf
@@ -2,7 +2,7 @@ resource "sysdig_secure_policy" "aws-policy-gitops" {
 
   name = "aws-policy-gitops"
   type = "aws_cloudtrail"
-  description = "custom managed policy operated with GitOps approach: GitHub actions + TF"
+  description = "Custom managed policy operated with GitOps approach: GitHub actions + TF"
   severity = 4
   enabled = true
   runbook = "https://runbook.com"
