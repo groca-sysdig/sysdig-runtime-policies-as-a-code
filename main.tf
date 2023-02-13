1fd01d239876cebf4c0c57453a82c63d5f6d68ad
groca-sysdig <105215194+groca-sysdig@users.noreply.github.com>
2023-02-13 13:22:30 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,11 +4,3 @@ provider "sysdig" {
   sysdig_secure_api_token = var.sysdig_api_token
 }
 
-resource "sysdig_secure_notification_channel_email" "sample_email" {
-    name                    = "Example Channel - Email"
-    recipients              = var.mail_users
-    enabled                 = var.enable_mail_notifications
-    notify_when_ok          = false
-    notify_when_resolved    = false
-    send_test_notification  = false
-}
