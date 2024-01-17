6849a3ac2230ba98a59a28b8c3666ab403046499
groca-sysdig <gonzalo.rocamador@sysdig.com>
2024-01-17 10:14:01 +0100

    tag changes on rules

--- falco-rules.tf
+++ falco-rules.tf
@@ -2,7 +2,7 @@
 resource "sysdig_secure_rule_falco" "falco_example_rule" {
   name        = "gitops - Terminal shell in container" // ID
   description = "A shell was used as the entrypoint/exec point into a container with an attached terminal."
-  tags        = ["gitops", "container", "shell", "mitre_execution"]
+  tags        = ["gitops", "hashitalk", "shell", "mitre_execution"]
 
   condition = "spawned_process and container and shell_procs and proc.tty != 0 and container_entrypoint"
   output    = "A shell was spawned in a container with an attached terminal (user=%user.name %container.info shell=%proc.name parent=%proc.pname cmdline=%proc.cmdline terminal=%proc.tty container_id=%container.id image=%container.image.repository)"
@@ -41,7 +41,7 @@ resource "sysdig_secure_rule_falco" "falco_example_rule" {
 resource "sysdig_secure_rule_falco" "falco_example_rule_2" {
   name        = "gitops - Connect EC2 Instance Metadata from container" // ID
   description = "Detect attempts to contact the EC2 Instance Metadata Service from a container"
-  tags        = ["gitops", "aws", "mitre_exfiltration"]
+  tags        = ["gitops", "aws", "hashitalk", "mitre_exfiltration"]
 
   condition = "outbound and fd.sip=\"169.254.169.254\" and ec2_metadata_containers"
   output    = "Outbound connection to EC2 instance metadata service (proc.cmdline=%proc.cmdline connection=%fd.name %container.info evt.type=%evt.type evt.res=%evt.res proc.pid=%proc.pid proc.cwd=%proc.cwd proc.ppid=%proc.ppid proc.pcmdline=%proc.pcmdline proc.sid=%proc.sid proc.exepath=%proc.exepath user.uid=%user.uid user.loginuid=%user.loginuid user.loginname=%user.loginname user.name=%user.name group.gid=%group.gid group.name=%group.name container.id=%container.id container.name=%container.name image=%container.image.repository:%container.image.tag)"
