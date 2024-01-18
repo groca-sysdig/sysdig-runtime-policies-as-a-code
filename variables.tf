3694a72297074f707a169b671062b1f0e6e54bc6
groca-sysdig <gonzalo.rocamador@sysdig.com>
2024-01-18 11:46:55 +0100

    change mail

--- variables.tf
+++ variables.tf
@@ -10,7 +10,7 @@ variable "enable_mail_notifications" {
 
 variable "mail_users" {
     type = list
-    default = ["testuser@mail.com"]
+    default = ["testuser@mail.com","hashi@talks.com"]
 
 }
 
