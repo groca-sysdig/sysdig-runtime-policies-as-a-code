f67387ce293e808726b945165cd9d6dbbdc1944d
groca-sysdig <gonzalo.rocamador@sysdig.com>
2024-03-11 09:33:58 +0100

    notif channel for my workload policy

--- policy-workload.tf
+++ policy-workload.tf
@@ -21,7 +21,7 @@ resource "sysdig_secure_policy" "workload-policy-hasthitalk" {
     }
   }
   
-  #  notification_channels = [10000]
+  notification_channels = [41677]
 ## This is to be added if you want to create rules at the same tie as policies
     depends_on = [
       sysdig_secure_rule_falco.falco_example_rule,
